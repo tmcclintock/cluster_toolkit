cc07987c1383d5967c87ff287a2e077424f57249
Tom McClintock <tmcclintock@email.arizona.edu>
2017-09-27 12:32:18 -0700

    Wrote documentation for the averaging code.

--- clusterwl/averaging.py
+++ clusterwl/averaging.py
@@ -1,11 +1,32 @@
 import clusterwl
 from ctypes import c_double, c_int, POINTER
 
-def dcast(x):
+def _dcast(x):
     return clusterwl._ffi.cast('double*', x.ctypes.data)
 
 def average_profile_in_bin(Rlow, Rhigh, R, prof):
-    return clusterwl._lib.average_profile_in_bin(Rlow, Rhigh, dcat(R), len(R), dcast(prof))
+    """Calculate the average of some projected profile in a radial bin in Mpc/h comoving
+
+    Args:
+    Rlow (float): Inner radii
+    Rhigh (float): Outer radii
+    R (array like): Radii of the profile
+    prof (array like): Projected profile
+
+    Returns:
+    ave_prof (float): Average profile in the radial bin, or annulus
+
+    """
+    return clusterwl._lib.average_profile_in_bin(Rlow, Rhigh, dcat(R), len(R), _dcast(prof))
 
 def average_profile_in_bins(Redges, R, prof, ave_prof):
-    return clusterwl._lib.average_profile_in_bins(dcast(Redges), len(Redges), dcast(R), len(R), dcast(prof), dcast(ave_prof))
+    """Calculate the average of some projected profile in a radial bins in Mpc/h comoving
+
+    Args:
+    Redges (array like): Array of radial bin edges
+    R (array like): Radii of the profile
+    prof (array like): Projected profile
+    ave_prof (float): Average profile in the radial bins, populated with the result
+
+    """
+    return clusterwl._lib.average_profile_in_bins(_dcast(Redges), len(Redges), _dcast(R), len(R), _dcast(prof), _dcast(ave_prof))
