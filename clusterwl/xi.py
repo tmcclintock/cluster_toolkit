551df65fda43492c25db65d7cde7f03686423866
Tom McClintock <tmcclintock@email.arizona.edu>
2017-09-27 11:05:39 -0700

    Made superfluous functions in xi.py underscore private.

--- clusterwl/xi.py
+++ clusterwl/xi.py
@@ -2,7 +2,7 @@ import clusterwl
 from ctypes import c_double, c_int, POINTER
 import numpy as np
 
-def dcast(x):
+def _dcast(x):
     return clusterwl._ffi.cast('double*', x.ctypes.data)
 
 def xi_nfw_at_R(R, M, c, om, delta=200):
@@ -21,7 +21,7 @@ def xi_nfw_at_R(R, M, c, om, delta=200):
     """
     if type(R) is list or np.ndarray:
         xi = np.zeros_like(R)
-        clusterwl._lib.calc_xi_nfw(dcast(R), len(R), M, c, delta, om, dcast(xi))
+        clusterwl._lib.calc_xi_nfw(_dcast(R), len(R), M, c, delta, om, _dcast(xi))
         return xi
     else:
         return clusterwl._lib.xi_nfw_at_R(R, M, c, delta, om)
@@ -42,9 +42,9 @@ def xi_mm_at_R(R, k, P, N=200, step=0.005):
     """
     if type(R) is list or np.ndarray:
         xi = np.zeros_like(R)
-        clusterwl._lib.calc_xi_mm(dcast(R), len(R), dcast(k), dcast(P), len(k), dcast(xi), N, step)
+        clusterwl._lib.calc_xi_mm(_dcast(R), len(R), _dcast(k), _dcast(P), len(k), _dcast(xi), N, step)
         return xi
-    return clusterwl._lib.xi_mm_at_R(R, dcast(k), dcast(P), len(k), N, step)
+    return clusterwl._lib.xi_mm_at_R(R, _dcast(k), _dcast(P), len(k), N, step)
 
 def xi_2halo(bias, xi_mm):
     """2-halo term in halo-matter correlation function
@@ -59,7 +59,7 @@ def xi_2halo(bias, xi_mm):
     """
     NR = len(xi_mm)
     xi = np.zeros_like(xi_mm)
-    clusterwl._lib.calc_xi_2halo(NR, bias, dcast(xi_mm), dcast(xi))
+    clusterwl._lib.calc_xi_2halo(NR, bias, _dcast(xi_mm), _dcast(xi))
     return xi
 
 def xi_hm(xi_1halo, xi_2halo):
@@ -76,10 +76,10 @@ def xi_hm(xi_1halo, xi_2halo):
 
     NR = len(xi_1halo)
     xi = np.zeros_like(xi_1halo)
-    clusterwl._lib.calc_xi_hm(NR, dcast(xi_1halo), dcast(xi_2halo), dcast(xi))
+    clusterwl._lib.calc_xi_hm(NR, _dcast(xi_1halo), _dcast(xi_2halo), _dcast(xi))
     return xi
 
-def calc_xi_nfw(R, M, c, om, xi, delta=200):
+def _calc_xi_nfw(R, M, c, om, xi, delta=200):
     """Direct call to the vectorized version of xi_nfw(R).
 
     Args:
@@ -91,10 +91,10 @@ def calc_xi_nfw(R, M, c, om, xi, delta=200):
     xi_nfw (float or array like): NFW correlation function, populated with the result
 
     """
-    clusterwl._lib.calc_xi_nfw(dcast(R), len(R), M, c, delta, om, dcast(xi))
+    clusterwl._lib.calc_xi_nfw(_dcast(R), len(R), M, c, delta, om, _dcast(xi))
     return
 
-def calc_xi_mm(R, k, P, xi, N=200, step=0.005):
+def _calc_xi_mm(R, k, P, xi, N=200, step=0.005):
     """Direct call to the vectorized version of xi_mm(R).
 
     Args:
@@ -107,10 +107,10 @@ def calc_xi_mm(R, k, P, xi, N=200, step=0.005):
 
     """
 
-    clusterwl._lib.calc_xi_mm(dcast(R), len(R), dcast(k), dcast(P), len(k), dcast(xi), N, step)
+    clusterwl._lib.calc_xi_mm(_dcast(R), len(R), _dcast(k), _dcast(P), len(k), _dcast(xi), N, step)
     return
 
-def calc_xi_2halo(bias, xi_mm, xi_2halo):
+def _calc_xi_2halo(bias, xi_mm, xi_2halo):
     """Direct call to the vectorized version of xi_2halo(R).
 
     Args:
@@ -121,10 +121,10 @@ def calc_xi_2halo(bias, xi_mm, xi_2halo):
     """
 
     NR = len(xi_mm)
-    clusterwl._lib.calc_xi_2halo(NR, bias, dcast(xi_mm), dcast(xi_2halo))
+    clusterwl._lib.calc_xi_2halo(NR, bias, _dcast(xi_mm), _dcast(xi_2halo))
     return
 
-def calc_xi_hm(xi_1halo, xi_2halo, xi_hm):
+def _calc_xi_hm(xi_1halo, xi_2halo, xi_hm):
     """Direct call to the vectorized version of xi_hm(R).
 
     Args:
@@ -135,5 +135,5 @@ def calc_xi_hm(xi_1halo, xi_2halo, xi_hm):
     """
 
     NR = len(xi_1halo)
-    clusterwl._lib.calc_xi_hm(NR, dcast(xi_1halo), dcast(xi_2halo), dcast(xi_hm))
+    clusterwl._lib.calc_xi_hm(NR, _dcast(xi_1halo), _dcast(xi_2halo), _dcast(xi_hm))
     return
