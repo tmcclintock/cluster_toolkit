53c68f49753a6aea3c07b3e42f7d240cedf8be95
Tom McClintock <tmcclintock89@gmail.com>
2017-11-19 23:51:04 -0700

    Working on requirements file.

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ class Mock(MagicMock):
     @classmethod
     def __getattr__(cls, name):
             return MagicMock()
-MOCK_MODULES = ['cluster_toolkit._lib',]
+MOCK_MODULES = ['cluster_toolkit._cluster_toolkit', 'cluster_toolkit._lib']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
