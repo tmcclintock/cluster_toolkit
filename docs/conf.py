ff88fdda58014ebdd4b19721e69e2c634660b9a8
Tom McClintock <tmcclintock89@gmail.com>
2017-11-19 23:08:14 -0700

    Still messing with the docs. at least they compile on rtd.

--- docs/conf.py
+++ docs/conf.py
@@ -16,16 +16,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../cluster_toolkit/bias.py'))
-#from mock import Mock as MagicMock
-#class Mock(MagicMock):
-#    @classmethod
-#    def __getattr__(cls, name):
-#            return MagicMock()
-#MOCK_MODULES = ['cluster_toolkit._lib', 'cluster_toolkit._ffi']
-#sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+#import os
+#import sys
+#sys.path.insert(0, os.path.abspath('../cluster_toolkit/bias.py'))
+
 
 
 # -- General configuration ------------------------------------------------
