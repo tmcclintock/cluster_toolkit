d67821e25b491251f408f538802a2cc80d961fd6
Tom McClintock <tmcclintock89@gmail.com>
2017-11-20 10:21:43 -0700

    Still working on docs.

--- cluster_toolkit/bias.py
+++ cluster_toolkit/bias.py
@@ -186,7 +186,26 @@ def _calc_bias_at_M(M, k, P, om, bias, delta=200):
     cluster_toolkit._lib.bias_at_M_arr(_dcast(M), len(M), delta, _dcast(k), _dcast(P), len(k), om, _dcast(bias))
     return
 
+def a_function(my_arg, another):
+    """
+    This is the brief description of my function to test.
+
+    This is a more complete example of my function. It can include doctest,
+    code blocks or any other reST structure.
+
+    >>> a_function(10, [MyClass('a'), MyClass('b')])
+    20
+
+    :param int my_arg: The first argument of the function. Just a number.
+    :param another: The other argument of the important function.
+    :type another: A list of :class:`MyClass`
+    :rtype: int
+    :return: The length of the second argument times the first argument.
+    """
+    return my_arg * len(another)
+
 __all__ = [
     'bias_at_M',
-    'bias_at_R'
+    'bias_at_R',
+    'a_function'
 ]
--- docs/cluster_toolkit.bias/cluster_toolkit.bias.rst
+++ docs/cluster_toolkit.bias/cluster_toolkit.bias.rst
@@ -19,7 +19,12 @@ Functions
 - :py:func:`bias_at_R`:
   Tinker 2008 bais at mass M [Msun/h] corresponding to radius R [Mpc/h comoving].
 
+- :py:func:`a_function`:
+  This is the brief description of my function to test.
+
 
 .. autofunction:: bias_at_M
 
 .. autofunction:: bias_at_R
+
+.. autofunction:: a_function
