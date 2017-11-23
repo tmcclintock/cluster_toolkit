016cc49d047b36de95f30f8039fb7a9005276ee5
Tom McClintock <tmcclintock89@gmail.com>
2017-11-23 07:25:49 -0700

    Another boostfactor test.

--- tests/test_boostfactors.py
+++ tests/test_boostfactors.py
@@ -7,10 +7,16 @@ import numpy.testing as npt
 R = np.logspace(-1,1.5,num=100)
 B0 = .19
 Rs = 1.0
+alpha = -1.0
 
 def test_nfw():
     npt.assert_array_less(1.0, bfs.boost_nfw_at_R(R, B0, Rs))
 
 def test_powerlaw():
-    alpha = -1.0
     npt.assert_array_less(1.0, bfs.boost_powerlaw_at_R(R, B0, Rs, alpha))
+
+def test_single_vs_array():
+    arrout = np.array([bfs.boost_nfw_at_R(Ri, B0, Rs) for Ri in R])
+    npt.assert_array_equal(arrout, bfs.boost_nfw_at_R(R, B0, Rs))
+    arrout = np.array([bfs.boost_powerlaw_at_R(Ri, B0, Rs, alpha) for Ri in R])
+    npt.assert_array_equal(arrout, bfs.boost_powerlaw_at_R(R, B0, Rs, alpha))
