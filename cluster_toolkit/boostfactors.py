8e88dd20f564fcc6480c7b0e28b1966988cfedd6
Tom McClintock <tmcclintock89@gmail.com>
2017-11-25 09:22:39 -0700

    More readability changes.

--- cluster_toolkit/boostfactors.py
+++ cluster_toolkit/boostfactors.py
@@ -5,13 +5,13 @@ import cluster_toolkit
 from cluster_toolkit import _dcast
 import numpy as np
 
-def boost_nfw_at_R(R, B0, Rs):
+def boost_nfw_at_R(R, B0, R_scale):
     """NFW boost factor model.
 
     Args:
-        R (float or array like): Distances on the sky in the same units as Rs. Mpc/h comoving suggested for consistency with other modules.
+        R (float or array like): Distances on the sky in the same units as R_scale. Mpc/h comoving suggested for consistency with other modules.
         B0 (float): NFW profile amplitude.
-        Rs (float): NFW profile scale radius.
+        R_scale (float): NFW profile scale radius.
 
     Returns:
         float or array like: NFW boost factor profile; B = (1-fcl)^-1.
@@ -19,18 +19,18 @@ def boost_nfw_at_R(R, B0, Rs):
     """
     if type(R) is list or type(R) is np.ndarray:
         boost = np.zeros_like(R)
-        cluster_toolkit._lib.boost_nfw_at_R_arr(_dcast(R), len(R), B0, Rs, _dcast(boost))
+        cluster_toolkit._lib.boost_nfw_at_R_arr(_dcast(R), len(R), B0, R_scale, _dcast(boost))
         return boost
     else:
-        return cluster_toolkit._lib.boost_nfw_at_R(R, B0, Rs)
+        return cluster_toolkit._lib.boost_nfw_at_R(R, B0, R_scale)
 
-def boost_powerlaw_at_R(R, B0, Rs, alpha):
+def boost_powerlaw_at_R(R, B0, R_scale, alpha):
     """Power law boost factor model.
 
     Args:
-        R (float or array like): Distances on the sky in the same units as Rs. Mpc/h comoving suggested for consistency with other modules.
+        R (float or array like): Distances on the sky in the same units as R_scale. Mpc/h comoving suggested for consistency with other modules.
         B0 (float): Boost factor amplitude.
-        Rs (float): Power law scale radius.
+        R_scale (float): Power law scale radius.
         alpha (float): Power law exponent.
 
     Returns:
@@ -39,7 +39,7 @@ def boost_powerlaw_at_R(R, B0, Rs, alpha):
     """
     if type(R) is list or type(R) is np.ndarray:
         boost = np.zeros_like(R)
-        cluster_toolkit._lib.boost_powerlaw_at_R_arr(_dcast(R), len(R), B0, Rs, alpha, _dcast(boost))
+        cluster_toolkit._lib.boost_powerlaw_at_R_arr(_dcast(R), len(R), B0, R_scale, alpha, _dcast(boost))
         return boost
     else:
-        return cluster_toolkit._lib.boost_powerlaw_at_R(R, B0, Rs, alpha)
+        return cluster_toolkit._lib.boost_powerlaw_at_R(R, B0, R_scale, alpha)
