73a81c902db10818c25df4580f8533183056a154
Tom McClintock <tmcclintock89@gmail.com>
2017-11-25 09:41:34 -0700

    More readability.

--- cluster_toolkit/deltasigma.py
+++ cluster_toolkit/deltasigma.py
@@ -5,14 +5,14 @@ import cluster_toolkit
 from cluster_toolkit import _dcast
 import numpy as np
 
-def Sigma_nfw_at_R(R, M, conc, om, delta=200):
+def Sigma_nfw_at_R(R, mass, concentration, Omega_m, delta=200):
     """Surface mass density of an NFW profile [Msun h/pc^2 comoving].
 
     Args:
         R (float or array like): Projected radii Mpc/h comoving.
-        M (float): Halo mass Msun/h.
-        conc (float): concentration.
-        om (float): Matter density fraction.
+        mass (float): Halo mass Msun/h.
+        concentration (float): concentration.
+        Omega_m (float): Matter density fraction.
         delta (int; optional): Overdensity, default is 200.
 
     Returns:
@@ -21,21 +21,21 @@ def Sigma_nfw_at_R(R, M, conc, om, delta=200):
     """
     if type(R) is list or type(R) is np.ndarray:
         Sigma = np.zeros_like(R)
-        cluster_toolkit._lib.Sigma_nfw_at_R_arr(_dcast(R), len(R), M, conc, delta, om, _dcast(Sigma))
+        cluster_toolkit._lib.Sigma_nfw_at_R_arr(_dcast(R), len(R), mass, concentration, delta, Omega_m, _dcast(Sigma))
         return Sigma
     else:
-        return cluster_toolkit._lib.Sigma_nfw_at_R(R, M, conc, delta, om)
+        return cluster_toolkit._lib.Sigma_nfw_at_R(R, mass, concentration, delta, Omega_m)
 
-def Sigma_at_R(R, Rxi, xi, M, conc, om, delta=200):
+def Sigma_at_R(R, Rxi, xi, mass, concentration, Omega_m, delta=200):
     """Surface mass density given some 3d profile [Msun h/pc^2 comoving].
 
     Args:
         R (float or array like): Projected radii Mpc/h comoving.
         Rxi (array like): 3D radii of xi_hm Mpc/h comoving.
         xi_hm (array like): Halo matter correlation function.
-        M (float): Halo mass Msun/h.
-        conc (float): concentration.
-        om (float): Matter density fraction.
+        mass (float): Halo mass Msun/h.
+        concentration (float): concentration.
+        Omega_m (float): Matter density fraction.
         delta (int; optional): Overdensity, default is 200.
 
     Returns:
@@ -44,22 +44,22 @@ def Sigma_at_R(R, Rxi, xi, M, conc, om, delta=200):
     """
     if type(R) is list or type(R) is np.ndarray:
         Sigma = np.zeros_like(R)
-        cluster_toolkit._lib.Sigma_at_R_full_arr(_dcast(R), len(R), _dcast(Rxi), _dcast(xi), len(Rxi), M, conc, delta, om, _dcast(Sigma))
+        cluster_toolkit._lib.Sigma_at_R_full_arr(_dcast(R), len(R), _dcast(Rxi), _dcast(xi), len(Rxi), mass, concentration, delta, Omega_m, _dcast(Sigma))
         return Sigma
     else:
-        return cluster_toolkit._lib.Sigma_at_R_full(R, _dcast(Rxi), _dcast(xi), len(Rxi), M, conc, delta, om)
+        return cluster_toolkit._lib.Sigma_at_R_full(R, _dcast(Rxi), _dcast(xi), len(Rxi), mass, concentration, delta, Omega_m)
 
 
-def DeltaSigma_at_R(R, Rs, Sigma, M, conc, om, delta=200):
+def DeltaSigma_at_R(R, Rs, Sigma, mass, concentration, Omega_m, delta=200):
     """Excess surface mass density given Sigma [Msun h/pc^2 comoving].
 
     Args:
         R (float or array like): Projected radii Mpc/h comoving.
         Rs (array like): Projected radii of Sigma, the surface mass density.
         Sigma (array like): Surface mass density.
-        M (float): Halo mass Msun/h.
-        conc (float): concentration.
-        om (float): Matter density fraction.
+        mass (float): Halo mass Msun/h.
+        concentration (float): concentration.
+        Omega_m (float): Matter density fraction.
         delta (int; optional): Overdensity, default is 200.
 
     Returns:
@@ -68,25 +68,25 @@ def DeltaSigma_at_R(R, Rs, Sigma, M, conc, om, delta=200):
     """
     if type(R) is list or type(R) is np.ndarray:
         DeltaSigma = np.zeros_like(R)
-        cluster_toolkit._lib.DeltaSigma_at_R_arr(_dcast(R), len(R), _dcast(Rs), _dcast(Sigma), len(Rs), M, conc, delta, om, _dcast(DeltaSigma))
+        cluster_toolkit._lib.DeltaSigma_at_R_arr(_dcast(R), len(R), _dcast(Rs), _dcast(Sigma), len(Rs), mass, concentration, delta, Omega_m, _dcast(DeltaSigma))
         return DeltaSigma
     else:
-        return cluster_toolkit._lib.DeltaSigma_at_R(R, _dcast(Rs), _dcast(Sigma), len(Rs), M, conc, delta, om)
+        return cluster_toolkit._lib.DeltaSigma_at_R(R, _dcast(Rs), _dcast(Sigma), len(Rs), mass, concentration, delta, Omega_m)
 
-def _calc_DeltaSigma_at_R(R, Rs, Sigma, M, conc, om, DeltaSigma, delta=200):
+def _calc_DeltaSigma_at_R(R, Rs, Sigma, mass, concentration, Omega_m, DeltaSigma, delta=200):
     """Direct call to vectorized excess surface mass density given Sigma [Msun h/pc^2 comoving]
 
     """
-    return cluster_toolkit._lib.DeltaSigma_at_R_arr(_dcast(R), len(R), _dcast(Rs), _dcast(Sigma), len(Rs), M, conc, delta, om, _dcast(DeltaSigma))
+    return cluster_toolkit._lib.DeltaSigma_at_R_arr(_dcast(R), len(R), _dcast(Rs), _dcast(Sigma), len(Rs), mass, concentration, delta, Omega_m, _dcast(DeltaSigma))
 
-def _calc_Sigma_nfw_at_R(R, M, conc, om, Sigma, delta=200):
+def _calc_Sigma_nfw_at_R(R, mass, concentration, Omega_m, Sigma, delta=200):
     """Direct call to vectorized surface mass density of NFW.
 
     """
-    return cluster_toolkit._lib.Sigma_nfw_at_R_arr(_dcast(R), len(R), M, conc, delta, om, _dcast(Sigma))
+    return cluster_toolkit._lib.Sigma_nfw_at_R_arr(_dcast(R), len(R), mass, concentration, delta, Omega_m, _dcast(Sigma))
 
-def _calc_Sigma_at_R(R, Rxi, xi, M, conc, om, Sigma, delta=200):
+def _calc_Sigma_at_R(R, Rxi, xi, mass, concentration, Omega_m, Sigma, delta=200):
     """Direct call to vectorized surface mass density given xi_hm
 
     """
-    return cluster_toolkit._lib.Sigma_at_R_full_arr(_dcast(R), len(R), _dcast(Rxi), _dcast(xi), len(Rxi), M, conc, delta, om, _dcast(Sigma))
+    return cluster_toolkit._lib.Sigma_at_R_full_arr(_dcast(R), len(R), _dcast(Rxi), _dcast(xi), len(Rxi), mass, concentration, delta, Omega_m, _dcast(Sigma))
