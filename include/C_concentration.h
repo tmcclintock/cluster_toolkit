fe558248cb90e9fd37b5f01da3b0fb0fab4db653
Tom McClintock <tmcclintock89@gmail.com>
2017-11-27 09:58:24 -0700

    Did a small amount of work on the DK15 concentration.

--- include/C_concentration.h
+++ include/C_concentration.h
@@ -1,1 +1,1 @@
-double DK15_concentration_at_M(double Mass, double*k, double*P, int Nk);
+double DK15_concentration_at_M(double Mass, double*k, double*P, int Nk, double Oemga_m);
--- src/C_concentration.c
+++ src/C_concentration.c
@@ -4,6 +4,7 @@
 #include <math.h>
 #include <stdio.h>
 
-double DK15_concentration_at_M(double Mass, double*k, double*P, int Nk){
+double DK15_concentration_at_M(double Mass, double*k, double*P, int Nk, double Omega_m){
+  double nu = nu_at_M(Mass, k, P, Nk, Omega_m);
   return 0;
 }
