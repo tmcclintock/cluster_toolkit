1e8e9f34b3cac7bda37fae9b74e96c540da0e600
Tom McClintock <tmcclintock89@gmail.com>
2017-12-01 07:33:32 -0700

    Added a unit test to test Rafael's issue.

--- tests/test_deltasigma.py
+++ tests/test_deltasigma.py
@@ -8,9 +8,13 @@ import numpy.testing as npt
 M = 1e14
 c = 5
 Om = 0.3
-here = dirname(__file__)
-Rxi = np.loadtxt(here+"/data_for_testing/r3d.txt")
-xihm = np.loadtxt(here+"/data_for_testing/xi_hm.txt")
+try:
+    here = dirname(__file__)
+    Rxi = np.loadtxt(here+"/data_for_testing/r3d.txt")
+    xihm = np.loadtxt(here+"/data_for_testing/xi_hm.txt")
+except IOError:
+    Rxi = np.loadtxt("./data_for_testing/r3d.txt")
+    xihm = np.loadtxt("./data_for_testing/xi_hm.txt")
 R = np.logspace(-1, 2, num = 1000)
 
 def test_Sigma_nfw():
@@ -38,3 +42,20 @@ def test_DeltaSigma():
     assert len(arrout) == len(R)
     for i in range(len(R)):
         npt.assert_equal(arrout[i], ds.DeltaSigma_at_R(R[i], R, sig, M, c, Om))
+
+def test_Sigma_and_xi():
+    #Test that changes to xi_hm below a cut only affect Sigma below that cut
+    arrout = ds.Sigma_at_R(R, Rxi, xihm, M, c, Om)
+    norm = arrout.copy()
+    xi2 = xihm.copy()
+    cut = 0.2
+    inds = np.where(Rxi < cut)[0]
+    xi2[inds] *= 2
+    arrout2 = ds.Sigma_at_R(R, Rxi, xi2, M, c, Om)
+    arrout /= norm
+    arrout2 /= norm
+    inds = np.where(R < cut)[0]
+    npt.assert_array_less(arrout[inds], arrout2[inds])
+    inds = np.where(R >= cut)[0]
+    #Splines mean that we won't get perfectly equal
+    npt.assert_array_almost_equal(arrout[inds], arrout2[inds], decimal=2)
