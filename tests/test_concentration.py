c0d4a394444f210c962f6a03b0cd58702458e93a
Tom McClintock <tmcclintock89@gmail.com>
2017-12-23 11:56:37 -0500

    Fixed some code in the concentration test file to make sure it can be run.

--- tests/test_concentration.py
+++ tests/test_concentration.py
@@ -5,9 +5,14 @@ import numpy as np
 import numpy.testing as npt
 
 #Halo properties that are inputs
+h = 0.7
+Omega_m = 0.3
+Omega_b = 0.05
+sigma8 = 0.847533481324
+ns = 0.96
+As = 2.215e-9
 rhomconst = 2.77533742639e+11 #units are SM h^2/Mpc^3
 Mass = 1e14 #Msun/h
-Omega_m = 0.3 #arbitrary
 datapath = "./data_for_testing/"
 k = np.loadtxt(join(dirname(__file__),datapath+"klin.txt")) #h/Mpc; wavenumber
 p = np.loadtxt(join(dirname(__file__),datapath+"plin.txt")) #[Mpc/h]^3 linear power spectrum
@@ -17,7 +22,6 @@ def test_exceptions():
     with pytest.raises(Exception):
         concentration.concentration_at_M(Mass, k, p, Omega_m, Mass_type="blah")
 
-        
 if __name__ == "__main__":
-    print concentration.concentration_at_M(Mass, k, p, Omega_m, Mass_type="crit")
-    print concentration.concentration_at_M(Mass, k, p, Omega_m, Mass_type="mean")
+    print concentration.concentration_at_M(Mass, k, p, ns, Omega_b, Omega_m, h, Mass_type="crit")
+    print concentration.concentration_at_M(Mass, k, p, ns, Omega_b, Omega_m, h, Mass_type="mean")
