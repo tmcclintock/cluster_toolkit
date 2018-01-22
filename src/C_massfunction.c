a4d923a8c29096a896ff91f4a6ec76ed21a96c9c
Tom McClintock <tmcclintock89@gmail.com>
2018-01-22 10:32:17 -0700

    Slight optimizations in the dndM functions.

--- src/C_massfunction.c
+++ src/C_massfunction.c
@@ -6,22 +6,45 @@
 #include "gsl/gsl_spline.h"
 #include <math.h>
 
-#define TOL 1e-6 //Used for the tinker bias
-#define rhomconst 2.77533742639e+11
+#define rhocrit 2.77533742639e+11
 //1e4*3.*Mpcperkm*Mpcperkm/(8.*PI*G); units are SM h^2/Mpc^3
-#define workspace_size 8000
-#define del 1e-8
+#define del 1e-6
 
-///////////////// G(sigma) multiplicity below ///////////////////
+///////////////// G multiplicity function///////////////////
+double G_at_M(double M, double*k, double*P, int Nk, double om, double d, double e, double f, double g){
+  double*Marr = (double*)malloc(sizeof(double));
+  double*G = (double*)malloc(sizeof(double));
+  double result;
+  Marr[0] = M;
+  G_at_M_arr(Marr, 1, k, P, Nk, om, d, e, f, g, G);
+  result = G[0];
+  free(Marr);
+  free(G);
+  return result;
+}
+
+int G_at_M_arr(double*M, int NM, double*k, double*P, int Nk, double om, double d, double e, double f, double g, double*G){
+  double*sigma = (double*)malloc(sizeof(double)*NM);
+  sigma2_at_M_arr(M, NM, k, P, Nk, om, sigma);
+  int i;
+  for(i = 0; i < NM; i++){
+    sigma[i] = sqrt(sigma[i]);
+  }
+  G_at_sigma_arr(sigma, NM, d, e, f, g, G);
+  free(sigma);
+  return 0;
+}
+
 double G_at_sigma(double sigma, double d, double e, double f, double g){
   double*sig = (double*)malloc(sizeof(double));
-  sig[0] = sigma;
   double*G = (double*)malloc(sizeof(double));
+  double result;
+  sig[0] = sigma;
   G_at_sigma_arr(sig, 1, d, e, f, g, G);
-  double res = G[0];
+  result = G[0];
   free(sig);
   free(G);
-  return res;
+  return result;
 }
 
 int G_at_sigma_arr(double*sigma, int Ns, double d, double e, double f, double g, double*G){
@@ -35,29 +58,6 @@ int G_at_sigma_arr(double*sigma, int Ns, double d, double e, double f, double g,
   for(i = 0; i < Ns; i++){
     G[i] = B*exp(-g/(sigma[i]*sigma[i]))*(pow(sigma[i]/e, -d)+pow(sigma[i], -f));
   }
-  return 0;
-}
-
-double G_at_M(double M, double*k, double*P, int Nk, double om, double d, double e, double f, double g){
-  double*Marr = (double*)malloc(sizeof(double));
-  Marr[0] = M;
-  double*G = (double*)malloc(sizeof(double));
-  G_at_M_arr(Marr, 1, k, P, Nk, om, d, e, f, g, G);
-  double res = G[0];
-  free(Marr);
-  free(G);
-  return res;
-}
-
-int G_at_M_arr(double*M, int NM, double*k, double*P, int Nk, double om, double d, double e, double f, double g, double*G){
-  double*sigma = (double*)malloc(sizeof(double)*NM);
-  sigma2_at_M_arr(M, NM, k, P, Nk, om, sigma);
-  int i;
-  for(i = 0; i < NM; i++){
-    sigma[i] = sqrt(sigma[i]);
-  }
-  G_at_sigma_arr(sigma, NM, d, e, f, g, G);
-  free(sigma);
   return 0;
 }
 
@@ -76,45 +76,38 @@ double dndM_at_M(double M, double*k, double*P, int Nk, double om, double d, doub
 }
 
 int dndM_at_M_arr(double*M, int NM, double*k, double*P, int Nk, double om, double d, double e, double f, double g, double*dndM){
-  double rhom = om*rhomconst;
-  //Compute all sigma(M) arrays
+  double rhom = om*rhocrit;
   double*M_top = (double*)malloc(sizeof(double)*NM);
   double*M_bot = (double*)malloc(sizeof(double)*NM);
   double*sigma = (double*)malloc(sizeof(double)*NM);
-  double*sigma_top = (double*)malloc(sizeof(double)*NM);
-  double*sigma_bot = (double*)malloc(sizeof(double)*NM);
+  double*sigma2_top = (double*)malloc(sizeof(double)*NM);
+  double*sigma2_bot = (double*)malloc(sizeof(double)*NM);
+  double*Gsigma = (double*)malloc(sizeof(double)*NM);
   int i;
-  double dM = 0;
+  double dlnsiginvdm;
   for(i = 0; i < NM; i++){
-    dM = del*M[i];
-    M_top[i] = M[i]-dM*0.5;
-    M_bot[i] = M[i]+dM*0.5;
+    M_top[i] = M[i]*(1-del*0.5);//M[i]-dM*0.5;
+    M_bot[i] = M[i]*(1+del*0.5);//M[i]+dM*0.5;
   }
   //Can reduce these to one function call by using a longer array
   sigma2_at_M_arr(M, NM, k, P, Nk, om, sigma);
-  sigma2_at_M_arr(M_top, NM, k, P, Nk, om, sigma_top);
-  sigma2_at_M_arr(M_bot, NM, k, P, Nk, om, sigma_bot);
+  sigma2_at_M_arr(M_top, NM, k, P, Nk, om, sigma2_top);
+  sigma2_at_M_arr(M_bot, NM, k, P, Nk, om, sigma2_bot);
   for(i = 0; i < NM; i++){
     sigma[i] = sqrt(sigma[i]);
-    sigma_top[i] = sqrt(sigma_top[i]);
-    sigma_bot[i] = sqrt(sigma_bot[i]);
   }
-  //Compute g(sigma)
-  double*Gsigma = (double*)malloc(sizeof(double)*NM);
-  G_at_sigma_arr(sigma, NM, d, e, f, g, Gsigma);
-  double*dlnsiginvdm = (double*)malloc(sizeof(double)*NM);
+  //Compute G(sigma), multiplicity function
+  G_at_sigma_arr(sigma, NM, d, e, f, g, Gsigma);  
   for(i = 0; i < NM; i++){
-    dM = del*M[i];
-    dlnsiginvdm[i] = log(sigma_top[i]/sigma_bot[i])/dM;
-    dndM[i] = Gsigma[i]*rhom/M[i]*dlnsiginvdm[i];
+    dlnsiginvdm = log(sigma2_top[i]/sigma2_bot[i])*0.5;
+    dndM[i] = Gsigma[i]*rhom*dlnsiginvdm/(del*M[i]*M[i]);
   }
   free(M_top);
   free(M_bot);
   free(sigma);
-  free(sigma_top);
-  free(sigma_bot);
-  free(Gsigma);
-  free(dlnsiginvdm);
+  free(sigma2_top);
+  free(sigma2_bot);
+  free(Gsigma);  
   return 0;
 }
 
@@ -123,13 +116,14 @@ int dndM_at_M_arr(double*M, int NM, double*k, double*P, int Nk, double om, doubl
 double n_in_bin(double Mlo, double Mhi, double*M, double*dndM, int NM){
   double*N = (double*)malloc(sizeof(double));
   double*edges = (double*)malloc(2*sizeof(double));
+  double result;
   edges[0] = Mlo;
   edges[1] = Mhi;
   n_in_bins(edges, 2, M, dndM, NM, N);
-  double res = N[0];
+  result = N[0];
   free(N);
   free(edges);
-  return res;
+  return result;
 }
 
 int n_in_bins(double*edges, int Nedges, double*M, double*dndM, int NM, double*N){
