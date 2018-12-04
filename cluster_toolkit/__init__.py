d5513539b5f724365a0823811553649afbcfd397
tmcclintock <tmcclintock89@gmail.com>
2018-12-04 13:38:09 -0500

    Fixed the bug related to compilation issues when using Python 3.

--- cluster_toolkit/__init__.py
+++ cluster_toolkit/__init__.py
@@ -12,6 +12,16 @@ __author__ = "Tom McClintock <mcclintock@bnl.gov>"
 cluster_toolkit_dir = os.path.dirname(__file__)
 include_dir = os.path.join(cluster_toolkit_dir,'include')
 lib_file = os.path.join(cluster_toolkit_dir,'_cluster_toolkit.so')
+# Some installation (e.g. Travis with python 3.x)
+# name this e.g. _cluster_toolkit.cpython-34m.so,
+# so if the normal name doesn't exist, look for something else.
+if not os.path.exists(lib_file):
+    alt_files = glob.glob(os.path.join(os.path.dirname(__file__),'_cluster_toolkit*.so'))
+    if len(alt_files) == 0:
+        raise IOError("No file '_cluster_toolkit.so' found in %s"%cluster_toolkit_dir)
+    if len(alt_files) > 1:
+        raise IOError("Multiple files '_cluster_toolkit*.so' found in %s: %s"%(cluster_toolkit_dir,alt_files))
+    lib_file = alt_files[0]
 
 _ffi = cffi.FFI()
 for file_name in glob.glob(os.path.join(include_dir,'*.h')):
