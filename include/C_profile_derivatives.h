da75e19c33adaf4fd91beb9218e11978407f01ff
tmcclintock <tmcclintock89@gmail.com>
2018-12-04 15:55:25 -0500

    Wrote but not tested yet the derivatives of xi_mm.

--- include/C_profile_derivatives.h
+++ include/C_profile_derivatives.h
@@ -1,5 +1,5 @@
 double drho_nfw_dr_at_R(double R, double M, double c, int delta, double Omega_m);
 int drho_nfw_dr_at_R_arr(double*R, int NR, double Mass, double conc, int delta, double Omega_m, double*drhodr);
 
-double dxi_mm_dr_at_R(double R, double*k, double*P, int Nk, int delta);
-double dxi_mm_dr_at_R_arr(double*R, int NR, double*k, double*P, int Nk, int delta);
+double dxi_mm_dr_at_R(double R, double*k, double*P, int Nk);
+int dxi_mm_dr_at_R_arr(double*R, int NR, double*k, double*P, int Nk, double*dxidr);
--- src/C_profile_derivatives.c
+++ src/C_profile_derivatives.c
@@ -21,6 +21,11 @@
 #define rhocrit 2.77533742639e+11
 //1e4*3.*Mpcperkm*Mpcperkm/(8.*PI*G); units are Msun h^2/Mpc^3
 
+#define workspace_size 8000
+#define workspace_num 100
+#define ABSERR 0.0
+#define RELERR 1.8e-4
+
 double drho_nfw_dr_at_R(double R, double Mass, double conc, int delta, double Omega_m){
   double*Rarr = (double*)malloc(sizeof(double));
   double*drhodr  = (double*)malloc(sizeof(double));
@@ -86,4 +91,88 @@ double integrand_dxi_mm_dr_SINE(double k, void*params){
   double x  = k*R;
   double P = get_P(x, R, kp, Pp, Nk, spline, acc);
   return P*k/(R*R); //Note - sin(kR) is taken care of in the qawo table
+}
+
+double dxi_mm_dr_at_R(double R, double*k, double*P, int Nk){
+  double*Rarr = (double*)malloc(sizeof(double));
+  double*dxidr  = (double*)malloc(sizeof(double));
+  double result;
+  Rarr[0] = R;
+  dxi_mm_dr_at_R_arr(Rarr, 1, k, P, Nk, dxidr);
+  result = dxidr[0];
+  free(Rarr);
+  free(dxidr);
+  return result;
+}
+
+int dxi_mm_dr_at_R_arr(double*R, int NR, double*k, double*P, int Nk, double*dxidr){
+  static int init_flag = 0;
+  static gsl_interp_accel*acc;
+  static gsl_integration_workspace*workspace;
+  static integrand_params_profile_derivs*params;
+  if (!init_flag){
+    acc = gsl_interp_accel_alloc();
+    workspace = gsl_integration_workspace_alloc(workspace_size);
+    params = malloc(sizeof(integrand_params_profile_derivs));
+    init_flag = 1;
+  }
+  
+  gsl_spline*Pspl = gsl_spline_alloc(gsl_interp_cspline, Nk);
+  gsl_integration_qawo_table*wf_cosine;
+  gsl_integration_qawo_table*wf_sine;
+ 
+  double kmax = 4e3;
+  double kmin = 5e-8;
+  double result_cosine, result_sine, err;
+  int i;
+  int status;
+  gsl_spline_init(Pspl, k, P, Nk);
+  params->acc = acc;
+  params->spline = Pspl;
+  params->kp = k;
+  params->Pp = P;
+  params->Nk = Nk;
+
+  gsl_function F_cosine;
+  gsl_function F_sine;
+
+  F_cosine.function = &integrand_dxi_mm_dr_COSINE;
+  F_sine.function   = &integrand_dxi_mm_dr_SINE;
+
+  F_cosine.params = params;
+  F_sine.params   = params;
+
+  wf_cosine = gsl_integration_qawo_table_alloc(R[0], kmax-kmin, GSL_INTEG_COSINE,
+					       (size_t)workspace_num);
+  wf_sine   = gsl_integration_qawo_table_alloc(R[0], kmax-kmin, GSL_INTEG_SINE,
+					       (size_t)workspace_num);
+  for(i = 0; i < NR; i++){
+    status = gsl_integration_qawo_table_set(wf_cosine, R[i], kmax-kmin, GSL_INTEG_COSINE);
+    status = gsl_integration_qawo_table_set(wf_sine, R[i], kmax-kmin, GSL_INTEG_SINE);
+    if (status){
+      printf("Error in dxi_mm_dr in qawo_table_set.\n");
+      exit(-1);
+    }
+    params->r=R[i];
+    status = gsl_integration_qawo(&F_cosine, kmin, ABSERR, RELERR, (size_t)workspace_num,
+				  workspace, wf_cosine, &result_cosine, &err);
+    status = gsl_integration_qawo(&F_sine, kmin, ABSERR, RELERR, (size_t)workspace_num,
+				  workspace, wf_sine, &result_sine, &err);
+    if (status){
+      printf("Error in dxi_mm_dr in the integrals.\n");
+      exit(-1);
+    }
+
+    dxidr[i] = (result_cosine - result_sine)/(M_PI*M_PI*2);
+  }
+
+  gsl_spline_free(Pspl);
+  gsl_integration_qawo_table_free(wf_cosine);
+  gsl_integration_qawo_table_free(wf_sine);
+  //These are now static.
+  //free(params);
+  //gsl_interp_accel_free(acc);
+  //gsl_integration_workspace_free(workspace);
+
+  return 0;
 }
