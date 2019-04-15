Fixed a factor of 2 bug that was cancelling out with another factor of 2 in everything up to now. I implemented dbias/dM and the bug showed up. All is fixed now.

cluster_toolkit/bias.py
@@ -115,9 +115,11 @@ def dbiasdM_at_M(M, k, P, Omega_m, delta=200):
     k = np.require(k, dtype=np.float64, requirements=["C"])
     P = np.require(P, dtype=np.float64, requirements=["C"])
     deriv = np.zeros_like(M)
-    cluster_toolkit._lib.dbiasdM_at_M_arr(_dcast(M), len(M), delta, _dcast(k), _dcast(P), len(k), Omega_m, _dcast(deriv))
+    cluster_toolkit._lib.dbiasdM_at_M_arr(_dcast(M), len(M), delta, _dcast(k),
+                                          _dcast(P), len(k), Omega_m,
+                                          _dcast(deriv))
     if scalar_input:
-        return np.squeeze(biasderiv)
+        return np.squeeze(deriv)
     return deriv
 
 def _bias_at_nu_FREEPARAMS(nu, A, a, B, b, C, c, delta=200):

cluster_toolkit/peak_height.py
@@ -85,7 +85,9 @@ def nu_at_R(R, k, P):
         return cluster_toolkit._lib.nu_at_R(R, _dcast(k), _dcast(P), len(k))
 
 def dsigma2dM_at_M(M, k, P, Omega_m):
-    """Derivative w.r.t. mass of RMS variance in top hat sphere of lagrangian radius R [Mpc/h comoving] corresponding to a mass M [Msun/h] of linear power spectrum.
+    """Derivative w.r.t. mass of RMS variance in top hat sphere of 
+    lagrangian radius R [Mpc/h comoving] corresponding to a mass 
+    M [Msun/h] of linear power spectrum.
 
     Args:
         M (float or array like): Mass in Msun/h.
@@ -99,16 +101,20 @@ def dsigma2dM_at_M(M, k, P, Omega_m):
     """
     if type(M) is list or type(M) is np.ndarray:
         ds2dM = np.zeros_like(M)
-        cluster_toolkit._lib.dsigma2dM_at_M_arr(_dcast(M), len(M), _dcast(k), _dcast(P), len(k), Omega_m, _dcast(ds2dM))
+        cluster_toolkit._lib.dsigma2dM_at_M_arr(_dcast(M), len(M), _dcast(k),
+                                                _dcast(P), len(k), Omega_m,
+                                                _dcast(ds2dM))
         return ds2dM
     else:
-        return cluster_toolkit._lib.dsigma2dM_at_M(M, _dcast(k), _dcast(P), len(k), Omega_m)
+        return cluster_toolkit._lib.dsigma2dM_at_M(M, _dcast(k), _dcast(P),
+                                                   len(k), Omega_m)
 
     return 0
 
     
 def _calc_sigma2_at_R(R, k, P, s2):
-    """Direct call to vectorized version of RMS variance in top hat sphere of radius R [Mpc/h comoving] of linear power spectrum.
+    """Direct call to vectorized version of RMS variance in top hat 
+    sphere of radius R [Mpc/h comoving] of linear power spectrum.
 
     """
     cluster_toolkit._lib.sigma2_at_R_arr(_dcast(R), len(R), _dcast(k), _dcast(P), len(k), _dcast(s2))

tests/test_bias.py
@@ -27,15 +27,23 @@ def test_exceptions_bias_at_M():
 
 def test_outputs_bias_at_M():
     #List vs. numpy.array
-    npt.assert_array_equal(bias.bias_at_M(Ma, klin, plin, Omega_m), bias.bias_at_M(Ma.tolist(), klin, plin, Omega_m))
+    npt.assert_array_equal(bias.bias_at_M(Ma, klin, plin, Omega_m),
+                           bias.bias_at_M(Ma.tolist(), klin, plin, Omega_m))
     #Single value vs numpy.array
     arrout = bias.bias_at_M(Ma, klin, plin, Omega_m)
     for i in range(len(Ma)):
         npt.assert_equal(bias.bias_at_M(Ma[i], klin, plin, Omega_m), arrout[i])
 
 def test_derivatives():
-    dbdM = bias.dbiasdM_at_M(Ma, klin, plin, Omega_m)
+    M = np.logspace(13, 14, 1000)
+    dbdM = bias.dbiasdM_at_M(M, klin, plin, Omega_m)
     npt.assert_equal(sorted(dbdM), dbdM[::-1])
+    b = bias.bias_at_M(M, klin, plin, Omega_m)
+    dM = M[1:] - M[:-1]
+    db = b[1:] - b[:-1]
+    deriv = db/dM
+    pd = dbdM[:-1] / deriv
+    npt.assert_array_almost_equal(pd, np.ones_like(pd), 1e-2)
     return
 
 def test_s2_and_nu_functions():